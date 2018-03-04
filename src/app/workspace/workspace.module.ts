544f8444eb096ad9730db990847da518febdb87f
Pedro Gomes <pedro.work.92@gmail.com>
2018-03-04 19:06:25 +0000

    Added component workspace

---
+++ src/app/workspace/workspace.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WorkspaceComponent } from './workspace.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [WorkspaceComponent]
+})
+export class WorkspaceModule { }
