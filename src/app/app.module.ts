fd2498a6506a9fc6d30d902516bd4f3a0e323c63
Pedro Gomes <pedro.work.92@gmail.com>
2018-03-04 13:25:53 +0000

    Added angular material with example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [
-    AppComponent
+	AppComponent
   ],
   imports: [
-    BrowserModule
+
+	// Browser
+	BrowserModule,
+
+	// Material
+	MatCardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
