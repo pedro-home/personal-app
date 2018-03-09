ed93db6c1cb2930273665378049bcea915d9df46
Pedro Gomes <pedro.work.92@gmail.com>
2018-03-09 21:29:11 +0000

    Added workspace-footer and workspace-page

---
+++ src/app/workspace/workspace-footer/workspace-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workspace-footer',
+  templateUrl: './workspace-footer.component.html',
+  styleUrls: ['./workspace-footer.component.scss']
+})
+export class WorkspaceFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/workspace/workspace-page/workspace-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workspace-page',
+  templateUrl: './workspace-page.component.html',
+  styleUrls: ['./workspace-page.component.scss']
+})
+export class WorkspacePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
