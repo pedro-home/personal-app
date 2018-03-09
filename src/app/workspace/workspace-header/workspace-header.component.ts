84eff42e67edd453679d44893edced3ec0435118
Pedro Gomes <pedro.work.92@gmail.com>
2018-03-09 21:30:25 +0000

    Reworked workspace-header to a more clean version

--- src/app/workspace/workspace-header/workspace-header.component.ts
+++ src/app/workspace/workspace-header/workspace-header.component.ts
@@ -1,47 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { WorkspaceHeaderService } from './workspace-header.service';
+import { Http } from '@angular/http';
 
 @Component({
 	selector: 'app-workspace-header',
 	templateUrl: './workspace-header.component.html',
-	styleUrls: ['./workspace-header.component.scss'],
-	providers: [WorkspaceHeaderService]
+	styleUrls: ['./workspace-header.component.scss']
 })
 export class WorkspaceHeaderComponent implements OnInit {
 	private header: WorkspaceHeader;
+	private data;
 	
-	constructor(private whService: WorkspaceHeaderService) {
-		this.header = new WorkspaceHeader();
+	constructor(private http:Http) {
+		this.http.get('./assets/messages/workspace/header.json')
+		.subscribe(res => this.header = new WorkspaceHeader(res.json()));
 	}
 
 	ngOnInit() {
+
 	}
 
 }
 class WorkspaceHeader {
-	private data: Object;
+	private data: JSON;
 
-	constructor() {
-		this.data = {
-			toolbar: [
-				{
-					icon: 'favorite'
-				},
-				{
-					name: 'About me'
-				},
-				{
-					name: 'Projects'
-				},
-				{
-					name: 'Contacts'
-				}
-			]
-		};
+	constructor(data: JSON) {
+		this.data = data;
 	}
 
 	public get toolbar(): Array<JSON> {
 		return this.data['toolbar'];
+	}
+
+	public get home(): string {
+		return this.data['home'];
 	}
 }
 
