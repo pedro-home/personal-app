bf1ad056878a5f5508ff1e573be5515f9b0fa23f
Pedro Gomes <pedro.work.92@gmail.com>
2018-03-09 21:30:47 +0000

    Support of messages JSON

--- src/app/workspace/workspace.module.ts
+++ src/app/workspace/workspace.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
 
 import { MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material';
 
 import { WorkspaceComponent } from './workspace.component';
 import { WorkspaceHeaderComponent } from './workspace-header/workspace-header.component';
+import { WorkspaceFooterComponent } from './workspace-footer/workspace-footer.component';
+import { WorkspacePageComponent } from './workspace-page/workspace-page.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
+	imports: [
+		CommonModule,
+		HttpModule,
 
-    // Material
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule
+		// Material
+		MatToolbarModule,
+		MatButtonModule,
+		MatIconModule
 
-  ],
-  exports: [WorkspaceComponent],
-  declarations: [WorkspaceComponent, WorkspaceHeaderComponent]
+	],
+	exports: [WorkspaceComponent],
+	declarations: [
+		WorkspaceComponent,
+		WorkspaceHeaderComponent,
+		WorkspaceFooterComponent,
+		WorkspacePageComponent
+	]
 })
 export class WorkspaceModule { }
