a953f7a51ff626284267aecc5235b73fd2b1c5c9
Pedro Gomes <pedro.work.92@gmail.com>
2018-03-10 01:44:41 +0000

    Added workspace-loader

---
+++ src/app/workspace/workspace-loader/workspace-loader.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-workspace-loader',
+	templateUrl: './workspace-loader.component.html',
+	styleUrls: ['./workspace-loader.component.scss']
+})
+export class WorkspaceLoaderComponent implements OnInit {
+	constructor() { }
+
+	ngOnInit() { }
+}
