22cc22033bb692b0d63c1631fe6fe372b9904673
Pedro Gomes <pedro.work.92@gmail.com>
2018-03-11 02:00:59 +0000

    Separated services in workspace.services

--- src/app/workspace/workspace.service.ts
+++ src/app/workspace/workspace.service.ts
@@ -4,6 +4,16 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class WorkspaceService {
+	public message: MessageService;
+	public dialog: DialogService;
+
+	constructor(http: Http) {
+		this.message = new MessageService(http);
+		this.dialog = new DialogService();
+	}
+}
+
+class MessageService {
 
 	private static MESSAGE_PATH: string = './assets/messages';
 	constructor(private http: Http) {
@@ -11,6 +21,10 @@ export class WorkspaceService {
 	}
 
 	public processMessage(path: string): any {
-		return this.http.get(`${WorkspaceService.MESSAGE_PATH}/${path}`);
+		return this.http.get(`${MessageService.MESSAGE_PATH}/${path}`);
 	}
+}
+
+class DialogService {
+
 }
