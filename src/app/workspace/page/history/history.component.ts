553058cba963499e0d2411fb3ded9ab01185f720
Pedro Gomes <pedro.work.92@gmail.com>
2018-03-27 18:11:29 +0100

    Styling fixes to history component

--- src/app/workspace/page/history/history.component.ts
+++ src/app/workspace/page/history/history.component.ts
@@ -10,33 +10,33 @@ export class HistoryComponent implements OnInit {
 	private size: number;
 	private distance: number;
 
-	private item = [
-		{
-			"year": ["2018", "now"],
-			"title": "HELLO",
-			"description": "FUCK YOU"
-		},
-		{
-			"year": ["2017", "2018"],
-			"title": "HELLO",
-			"description": "FUCK YOU"
-		},
-		{
-			"year": ["2016", "2017"],
-			"title": "HELLO",
-			"description": "FUCK YOU"
-		},
-		{
-			"year": ["2014", "2016"],
-			"title": "HELLO",
-			"description": "FUCK YOU"
-		},
-		{
-			"year": ["2012", "2014"],
-			"title": "A baby is born",
-			"description": "FUCK YOU"
-		}
-	]
+	private item =
+	{
+		"birth": "1992",
+		"milestones":
+		[
+			{
+				"year": ["2018", "now"],
+				"title": "HELLO",
+				"description": "FUCK YOU"
+			},
+			{
+				"year": ["2017", "2018"],
+				"title": "HELLO",
+				"description": "FUCK YOU"
+			},
+			{
+				"year": ["2016", "2017"],
+				"title": "HELLO",
+				"description": "FUCK YOU"
+			},
+			{
+				"year": ["2014", "2016"],
+				"title": "HELLO",
+				"description": "FUCK YOU"
+			}
+		]
+	}
 
 	constructor() {
 		this.size = 14;
