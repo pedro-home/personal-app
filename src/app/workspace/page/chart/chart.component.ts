d9da9a70c73083c06bce06c29b9df0b38d6aac3e
Pedro Gomes <pedro.work.92@gmail.com>
2018-03-27 23:14:35 +0100

    Styling fixes to chart component

--- src/app/workspace/page/chart/chart.component.ts
+++ src/app/workspace/page/chart/chart.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-chart',
-  templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.scss']
+	selector: 'app-chart',
+	templateUrl: './chart.component.html',
+	styleUrls: ['./chart.component.scss']
 })
 export class ChartComponent implements OnInit {
 
-  constructor() { }
+	private size: number;
+	private distance: number;
 
-  ngOnInit() {
-  }
+	private item =
+	{
+		"bars":
+		[
+			{
+				"name": "TYPESCRIPT, ECMASCRIPT & JAVASCRIPT",
+				"value": 75
+			},
+			{
+				"name": "HTML5",
+				"value": 70
+			},
+			{
+				"name": "SCSS & CSS",
+				"value": 80
+			},
+			{
+				"name": "NODEJS",
+				"value": 60
+			}
+		]
+	}
+
+	constructor() {
+		this.size = 1000;
+		this.distance = 25;
+	}
+
+	ngOnInit() {
+	}
 
 }
