d7fb56e9f8a5f0d00ad407ba5fc147a7bfaa3a40
Pedro Gomes <pedro.work.92@gmail.com>
2018-04-01 23:53:31 +0100

    Added toolbar animation

--- src/app/workspace/header/header.component.ts
+++ src/app/workspace/header/header.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { BindableComponent } from '../base/base';
+import { RendererService } from '../renderer.service';
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
-	styleUrls: ['./header.component.scss']
+	styleUrls: ['./header.component.scss'],
+	providers: [RendererService]
 })
-export class HeaderComponent extends BindableComponent {
+export class HeaderComponent extends BindableComponent implements OnInit {
 
+	private shrinkToolbar: boolean;
+
+	constructor(private rendererService: RendererService) {
+		super();
+
+		this.shrinkToolbar = false;
+	}
+
+	ngOnInit(): void {
+		this.rendererService.on('window', 'scroll', (event: any) => {
+			let doc = event.target.documentElement;
+			this.shrinkToolbar = (doc.scrollTop > doc.clientHeight/2);
+		});
+	}
 }
 
