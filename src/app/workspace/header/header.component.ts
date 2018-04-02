ea8dae4a0bc095c3bf7f457990b4cf8abc9e6897
Pedro Gomes <pedro.work.92@gmail.com>
2018-04-03 00:21:05 +0100

    Styling corrections and added more animations

--- src/app/workspace/header/header.component.ts
+++ src/app/workspace/header/header.component.ts
@@ -12,17 +12,43 @@ import { DomService } from '../dom.service';
 export class HeaderComponent extends BindableComponent implements OnInit {
 
 	private showToolbar: boolean;
+	private highlightButton: boolean;
 
 	constructor(private rendererService: RendererService, private domService: DomService) {
 		super();
 
 		this.showToolbar = false;
+		this.highlightButton = false;
 	}
 
 	ngOnInit(): void {
+
+		// TODO Increase performance on scroll event
 		this.rendererService.on('window', 'scroll', (event: any) => {
-			let doc = this.domService.document.documentElement;
-			this.showToolbar = (doc.scrollTop > doc.clientHeight/2);
+			let doc = this.domService.document;
+			let docElem = doc.documentElement;
+			this.showToolbar = (docElem.scrollTop > docElem.clientHeight/2);
+
+			let id, elem;
+			let threshold = (docElem.scrollTop + docElem.clientHeight/4);
+			let fullHeight = docElem.offsetHeight;
+			for (let button of this.model.data['buttons'])
+			{
+				button['highlight'] = false;
+
+				id = button['action'];
+				if (id[0] === '#')
+				{
+					elem = doc.getElementById(id.substr(1));
+
+					if ((threshold >= elem.offsetTop &&
+						threshold <= elem.offsetTop + elem.clientHeight) ||
+						(docElem.scrollTop + docElem.clientHeight === fullHeight &&
+						elem.offsetTop + elem.clientHeight === fullHeight)) {
+						button['highlight'] = true;
+					}
+				}
+			}
 		});
 	}
 }
